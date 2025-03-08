8d53f92111c4bc1a074d8e7a01f5f50c9fb29db8
David Chakanza <dchakanza@gmail.com>
2025-03-08 11:32:54 +0200

    Update entrypoint.sh

--- farmer-chat/entrypoint.sh
+++ farmer-chat/entrypoint.sh
@@ -2,16 +2,26 @@
 # set -e
 
 echo "Running entrypoint.sh"
+
 # Source the environment variable file
 touch /app/uvicorn_logs.txt
 touch /app/celery_pid.pid
 touch /app/celery_logs.txt
 tail -n 0 -f /app/uvicorn_logs.txt &
+
 cd /app
 
+# Set default port if not provided
+PORT=${PORT:-8000}
+
 # Gunicorn run
-/opt/venv/bin/gunicorn --workers=4 -k uvicorn.workers.UvicornH11Worker --bind 0.0.0.0:8000 -m 007 --log-level debug --access-logfile /app/uvicorn_logs.txt --error-logfile /app/uvicorn_logs.txt --log-file /app/uvicorn_logs.txt --capture-output django_core.wsgi:application &
-/opt/venv/bin/celery -A django_core worker -l DEBUG -c 16 --max-tasks-per-child=2 --pidfile /app/celery_pid.pid  --logfile /app/celery_logs.txt &
+/opt/venv/bin/gunicorn --workers=4 --bind 0.0.0.0:$PORT --log-level debug \
+    --access-logfile /app/uvicorn_logs.txt --error-logfile /app/uvicorn_logs.txt \
+    --log-file /app/uvicorn_logs.txt --capture-output django_core.wsgi:application &
+
+# Start Celery worker
+/opt/venv/bin/celery -A django_core worker -l DEBUG -c 16 --max-tasks-per-child=2 \
+    --pidfile /app/celery_pid.pid --logfile /app/celery_logs.txt &
 
 wait
 
