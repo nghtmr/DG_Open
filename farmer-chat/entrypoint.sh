c6f5b09f6e579b21cc3adca4219f78c3487373c3
David Chakanza <dchakanza@gmail.com>
2025-03-08 11:15:10 +0200

    Update entrypoint.sh

--- farmer-chat/entrypoint.sh
+++ farmer-chat/entrypoint.sh
@@ -10,7 +10,7 @@ tail -n 0 -f /app/uvicorn_logs.txt &
 cd /app
 
 # Gunicorn run
-/opt/venv/bin/gunicorn --workers=4 -k uvicorn.workers.UvicornH11Worker --bind 0.0.0.0:8000 -m 007 --log-level debug --access-logfile /app/uvicorn_logs.txt --error-logfile /app/uvicorn_logs.txt --log-file /app/uvicorn_logs.txt --capture-output django_core.asgi:application &
+/opt/venv/bin/gunicorn --workers=4 -k uvicorn.workers.UvicornH11Worker --bind 0.0.0.0:8000 -m 007 --log-level debug --access-logfile /app/uvicorn_logs.txt --error-logfile /app/uvicorn_logs.txt --log-file /app/uvicorn_logs.txt --capture-output django_core.wsgi:application &
 /opt/venv/bin/celery -A django_core worker -l DEBUG -c 16 --max-tasks-per-child=2 --pidfile /app/celery_pid.pid  --logfile /app/celery_logs.txt &
 
 wait
