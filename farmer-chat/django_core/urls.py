7f569a0492424d2a9f842701a06a1a008824e2f6
David Chakanza <dchakanza@gmail.com>
2025-03-08 12:41:06 +0200

    Update urls.py

--- farmer-chat/django_core/urls.py
+++ farmer-chat/django_core/urls.py
@@ -1,13 +1,15 @@
 """django_core URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 """
-
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    
+    # Redirect root to the API root
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
 ]
